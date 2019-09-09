ee936036911e918b4755eb123444fb99f9de9f29
ripon630371 <riponcmt630371@gmail.com>
2019-09-09 14:11:09 +0600

    ৩.৮ - পোস্টের সিংগেল ভিউ এবং থিমের কোড আপডেট

--- index.php
+++ index.php
@@ -25,7 +25,11 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h2 class="post-title"><?php the_title(); ?></h2>
+                    <h2 class="post-title">
+
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                            
+                    </h2>
                 </div>
             </div>
             <div class="row">
@@ -45,7 +49,14 @@
                             }
                         ?>
                     </p>
-                    <?php the_excerpt(); ?>
+
+                  <?php if(is_single()){
+                    the_content();
+                  }else{
+                    the_excerpt();
+                  }
+                  ?>
+                    
                 </div>
             </div>
 
